3cad9306ce0d836b734c6926c909a5fe2344acfe
Giovanna Azzolini <giovannadev997@gmail.com>
2025-11-21 19:19:55 -0300

    feat: new cluster module

---
+++ modules/cluster/output.tf
@@ -0,0 +1,5 @@
+output "oidc" {
+  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+
--- variables.tf
+++ variables.tf
@@ -7,4 +7,3 @@ variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
 }
-
